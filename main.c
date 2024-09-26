f29c1ab51795e6f18e8029be028f196457a1f976
SergeyRyabykin <sryabykin@bk.ru>
2024-09-27 01:09:38 +0300

    main.c was changed to fit the latest modifications

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
+#include "led.h"
+#include "uart.h"
+#include "nvic.h"
+
+#define COUNT_VAL 0xfffff
 
 int main(void) 
 {
+  led_init();
+  uart_init();
+  nvic_enable_uart2_irq();
+
+
   while(1) {
-    ;
+    volatile unsigned long i = 0;
+
+    for(i = 0; i < COUNT_VAL; ) {
+      i++;
+    }
+    uart_putc(0x30);
   }
 
   return 0;
@@ -11,4 +26,11 @@ int main(void)
 void SystemInit(void)
 {
   ;
+}
+
+void USART2_IRQHandler(void)
+{
+  nvic_clear_uart2_irq();
+  uart_clear_irq();
+  led_toggle();
 }
