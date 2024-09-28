1ee2335c7fd17db3b2cb9a5815cd05a97369cde3
SergeyRyabykin <sryabykin@bk.ru>
2024-09-28 15:11:50 +0300

    dma added to nvic

--- code/inc/nvic.h
+++ code/inc/nvic.h
@@ -4,4 +4,7 @@
 void nvic_enable_uart2_irq(void);
 void nvic_clear_uart2_irq(void);
 
+void nvic_enable_dma1_ch7_irq(void);
+void nvic_clear_dma1_ch7_irq(void);
+
 #endif /* NVIC_H */
--- code/src/nvic.c
+++ code/src/nvic.c
@@ -2,6 +2,7 @@
 #include "common.h"
 
 #define NVIC_UART2_IRQ_NUM 38
+#define NVIC_DMA1_CH7_IRQ_NUM 17
 
 #define NVIC_ISER_BASE 0xE000E100
 #define NVIC_IPR_BASE  0xE000E400
@@ -28,4 +29,15 @@ void nvic_enable_uart2_irq(void)
 void nvic_clear_uart2_irq(void)
 {
     NVIC_CLEAR_IRQ(NVIC_UART2_IRQ_NUM);
+}
+
+void nvic_enable_dma1_ch7_irq(void)
+{
+    NVIC_SET_PRIORITY(NVIC_DMA1_CH7_IRQ_NUM, 1);
+    NVIC_ENABLE_IRQ(NVIC_DMA1_CH7_IRQ_NUM);
+}
+
+void nvic_clear_dma1_ch7_irq(void)
+{
+    NVIC_CLEAR_IRQ(NVIC_DMA1_CH7_IRQ_NUM);
 }
